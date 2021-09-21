6869e7206af65657f707970e74301fa81eba5e10
yamaoka-aosaba <89884929+yamaoka-aosaba@users.noreply.github.com>
2021-09-22 04:08:48 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,12 +23,17 @@ const app = Vue.createApp({
         stage2: {
           q1: 'いいい',
           // q2: 'えええ',
-          // q3: 'おおお'
+          // q3: 'おおお',
         },
         stage3: {
           q1: 'ううう',
           // q2: 'かかか',
           // q3: 'ききき',
+        },
+        stage4: {
+          q1: 'よんもんめ',
+          // q2: 'くくく',
+          // q3: 'けけけ',
         }
       },
 
@@ -48,6 +53,11 @@ const app = Vue.createApp({
           false, // 3-1
           // false, // 3-2
           // false, // 3-3
+        ],
+        stage4: [
+          false, // 4-1
+          // false, // 4-2
+          // false, // 4-3
         ]
       },
 
@@ -56,6 +66,7 @@ const app = Vue.createApp({
         stage1: false,
         stage2: false,
         stage3: false,
+        stage4: false,
       },
 
       /* 次のステージを表示するかどうか
@@ -64,6 +75,7 @@ const app = Vue.createApp({
       next: {
         stage1: false,
         stage2: false,
+        stage3: false,
       },
     }
   },
